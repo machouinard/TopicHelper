8f3b647e90869881a231c10f18eb41bf769232ef
Mark Chouinard <mark@chouinard.me>
2019-09-05 17:50:34 -0700

    CoreDataStack

--- TopicHelper/AppDelegate.swift
+++ TopicHelper/AppDelegate.swift
@@ -13,6 +13,7 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    lazy var coreDataStack = CoreDataStack(modelName: "TopicHelper")
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -22,31 +23,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         if let tabViewControllers = tabController.viewControllers {
             let topicNavController = tabViewControllers[0] as! UINavigationController
             let topicVC = topicNavController.viewControllers.first as! ViewController
-            topicVC.managedContext = persistentContainer.viewContext
+            topicVC.managedContext = coreDataStack.managedContext
             let allTopicsNavController = tabViewControllers[1] as! UINavigationController
             let allTopicsVC = allTopicsNavController.viewControllers.first as! AllTopicsViewController
-            allTopicsVC.managedContext = persistentContainer.viewContext
+            allTopicsVC.managedContext = coreDataStack.managedContext
         }
-        
-        
-//        guard let navController = window?.rootViewController as? UINavigationController,
-//            let viewController = navController.topViewController as? ViewController else {
-//                return true
-//        }
-//
-//        viewController.managedContext = persistentContainer.viewContext
-        
-//        guard let nc = window?.rootViewController?.children.first?.presentedViewController as? ViewController else {
-//            return true
-//        }
-//
-//        nc.managedContext = persistentContainer.viewContext
-        
-//        guard let vc = window?.rootViewController as? ViewController else {
-//            return true
-//        }
-//
-//        vc.managedContext = persistentContainer.viewContext
         
         return true
     }
@@ -59,6 +40,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        coreDataStack.saveContext()
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -72,52 +54,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
-        self.saveContext()
-    }
-
-    // MARK: - Core Data stack
-
-    lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-        */
-        let container = NSPersistentContainer(name: "TopicHelper")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-
-    // MARK: - Core Data Saving support
-
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
+        coreDataStack.saveContext()
     }
 
 }
