3abaa8417955216c5de1c3d6f0017188b411bb3b
Mark Chouinard <mark@chouinard.me>
2019-10-05 00:13:35 -0700

    Improve UI, better tableCell layout.

--- TopicHelper/Views/TopicCell.swift
+++ TopicHelper/Views/TopicCell.swift
@@ -27,19 +27,19 @@ class TopicCell: UITableViewCell {
     // MARK: - Helper methods
     func configure(for topic: Topic) {
         
-        self.layoutMargins = UIEdgeInsets.zero
-        self.contentView.layoutMargins.right = 20
+//        self.layoutMargins = UIEdgeInsets.zero
+//        self.contentView.layoutMargins.right = 20
         
         // Add and configure Favorite button
         let fave = self.viewWithTag(331) as! UIButton
         fave.addTarget(nil, action: #selector(TopicsViewController.toggleFavorite(_:)), for: .touchUpInside)
 //        fave.translatesAutoresizingMaskIntoConstraints = false
-        fave.frame = CGRect(x: 0, y: 0, width: 20, height: 20)
-        fave.contentEdgeInsets = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
-        fave.imageView?.contentMode = .scaleAspectFit
-        fave.titleLabel?.text = ""
+//        fave.frame = CGRect(x: 0, y: 0, width: 15, height: 15)
+//        fave.contentEdgeInsets = UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+//        fave.imageView?.contentMode = .scaleAspectFit
+//        fave.titleLabel?.text = ""
 
-        fave.tintColor = .white
+        fave.tintColor = .systemBlue
         
         var imageName = String()
         
